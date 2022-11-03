e7c8710f0d9b7a5fe5cfa8026c006ab4f10182f9
Brenda Liu <brenda.liu24@gmail.com>
2022-11-03 14:38:29 -0500

    complete problem 2131

---
+++ problem-2131.js
@@ -0,0 +1,45 @@
+var longestPalindrome = function(words) {
+  if(!words.length) return 0;
+  // if(words.length === 1) return 2;
+
+  const nonSamePairs = {};
+  const samePairs = {};
+
+  for (let word of words) {
+    if(word[0] === word[1]) {
+      samePairs[word] ? samePairs[word]++ : samePairs[word] = 1;
+    } else {
+      nonSamePairs[word] ? nonSamePairs[word]++ : nonSamePairs[word] = 1;
+    }
+  }
+
+  let longest = 0;
+  let oneOdd = false;
+
+  for(let pair in samePairs) {
+    if(samePairs[pair] % 2 === 0) {
+      longest += samePairs[pair] * 2;
+    } else if (samePairs[pair] % 2 === 1 && !oneOdd) {
+      longest += samePairs[pair] * 2;
+      oneOdd = true;
+    } else if (samePairs[pair] % 2 === 1 && oneOdd) {
+      longest += (samePairs[pair] - 1) * 2;
+    }
+  }
+
+  // if (!Object.keys(nonSamePairs).length) return longest;
+
+  for (let pair in nonSamePairs) {
+    let reflection = pair[1] + pair[0];
+    if (nonSamePairs[reflection]){
+      longest += 4;
+      nonSamePairs[pair]--;
+      nonSamePairs[reflection]--;
+
+      if(nonSamePairs[pair] === 0) delete nonSamePairs[pair];
+      if(nonSamePairs[reflection] === 0) delete nonSamePairs[reflection];
+    }
+  }
+
+  return longest;
+};
