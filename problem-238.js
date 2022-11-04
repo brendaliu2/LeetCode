fd6504b5badb95949169a5dfe8604b6f3e08235d
Brenda Liu <brenda.liu24@gmail.com>
2022-11-04 13:04:00 -0500

    use exponents to solve 238

--- problem-238.js
+++ problem-238.js
@@ -17,3 +17,33 @@ var productExceptSelf = function(nums) {
 
   return answer;
 };
+
+//andrew method
+var productExceptSelf = function(nums) {
+  const answer = [];
+
+  const zeros = nums.filter(n => !n);
+  if(zeros.length > 1) return new Array(nums.length).fill(0);
+
+  let product = 1;
+  let zeroLocation;
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i]) {
+      product *= nums[i];
+    } else {
+      zeroLocation = i;
+    }
+  }
+
+  if(zeros.length === 1) {
+    const products = new Array(nums.length).fill(0);
+    products[zeroLocation] = product;
+    return products;
+  }
+
+  for(let num of nums) {
+    answer.push(product * num**-1);
+  }
+
+  return answer;
+};
